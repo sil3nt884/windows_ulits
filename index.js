51a2ada6381af5a230d29509af51fb81ff95725e
Ricky Luu <ricky.luu911@gmail.com>
2023-01-10 15:07:19 +0000

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const screenshot = require('src/screenshot')
-const keypress = require('src/keyPress')
+const screenshot = require('./src/screenshot')
+const keypress = require('./src/keyPress')
 module.exports  = {
     screenshot,
     keypress
 }
+
+screenshot.takeScreenshot()
